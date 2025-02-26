4dda89c0eec699c4c9333fbf0be6b8eb8f682d20
Framebuffer <26495556+Framebuffers@users.noreply.github.com>
2025-02-26 15:43:40 -0300

    Add project files.

---
+++ Direwolf/ManifestGenerator/ManifestAttributes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Microsoft.XmlSerializer.Generator;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+
+namespace Direwolf.ManifestGenerator
+{
+
+    /// <summary>
+    /// Attribute to automatically generate all neccesary data to create a valid Autodesk Revit .addin XML manifest. Adding these will ensure your add-in will be loaded by Revit successfully.
+    /// </summary>       
+    /// <param name="revitAddinInstallationPath">
+    /// Fill the <b>absolute path</b> for your Addins folder. Unless it's been specified otherwise when Revit was installed, it should be:
+    /// <code>C:\ProgramData\Autodesk\Revit\Addins\20xx\</code> More official documentation is available at <see href="https://www.autodesk.com/support/technical/article/caas/tsarticles/ts/7JiiE4UoWHaTjxzuF754mL.html"/>.
+    /// </param>
+    /// <param name="vendorName">
+    /// Name of the company (or developer) creating this add-in.
+    /// Defaults to "unknown"
+    /// </param>
+    /// <param name="addinName">
+    /// The name upon which your add-in will be called inside Revit.
+    /// </param>       
+    /// <param name="addinDescription">
+    /// A string that appears when a user mouses over your add-in's button.
+    /// </param>
+    /// <param name="revitDiscipline">
+    /// Add-ins can be separated by each Revit Discipline. Don't know how relevant it is on Revit 2025+
+    /// </param>
+    /// <param name="locale">
+    /// The language used by your add-in. Should be upgraded to use native .NET i18n facilities.
+    /// </param>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ManifestAttributes(string revitAddinInstallationPath,
+                              string vendorName = "unknown",
+                              string vendorDescription = "unknown",
+                              string addinName = "unknown",
+                              string addinDescription = "unknown",
+                              string revitDiscipline = "unknown",
+                              string locale = "unknown",
+                              bool alwaysVisible = true) : System.Attribute
+    {
+        private Guid ExtensionGuid => new();
+    }
+}
---
+++ Forest/Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Forest.MockAttributesTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestClass t = new();
+            t.HelloWorld();
+        }
+
+        // Mocks
+        [Transaction(TransactionMode.Manual)]
+        [Construction(TransactionMode.Manual)]
+        internal class TestClass
+        {
+            public void HelloWorld() => System.Console.WriteLine("Hello, from within the test class!");
+            
+            // code from within the attributes is ran first.
+            public TestClass()
+            {
+                Console.WriteLine("Listing all custom attributes:");
+                foreach (var attr in typeof(TestClass).GetCustomAttributes())
+                {
+                    Console.WriteLine(attr.ToString());
+                }
+
+
+            }
+        }
+    }
+
+    // Mocks
+    internal enum TransactionMode
+    {
+        Manual = 1,
+        ReadOnly
+    }
+    internal class TransactionAttribute : Attribute
+    {
+        private TransactionMode Mode { get; set; }
+        public TransactionAttribute(TransactionMode mode)
+        {
+            Mode = mode;
+            Console.WriteLine("Running inside the attribute: Transaction Attached!");
+        }
+    }
+
+    internal class ConstructionAttribute : Attribute
+    {
+        public ConstructionAttribute(TransactionMode t)
+        {
+            //var check = this.GetType().GetCustomAttributes().OfType<TransactionAttribute>().Any();
+            //if (check)
+            //{
+            //    Console.WriteLine("Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not found");
+            //}
+
+            Console.WriteLine($"Running inside the attribute: Construction Attached! Mode is {t}");
+        }
+    }
+}
