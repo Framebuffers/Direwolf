98117a4e4c8a585b88544c1a68de70f1a3b8fef3
Framebuffer <26495556+Framebuffers@users.noreply.github.com>
2025-03-03 22:52:29 -0300

    Added Benchmark data

--- Direwolf/Main.cs
+++ Direwolf/Main.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Direwolf
 {
@@ -28,72 +29,100 @@ namespace Direwolf
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            RevitTask.Initialize(commandData.Application);
+            // This is a benchmark of data extraction between a native syncronous Revit Command, and a Direwolf asyncronous Command.
+            // The one difference from any other Revit.Async command, is that Direwolf has a special data structure.
+            // Here's a native Revit command:
+
             Document doc = commandData.Application.ActiveUIDocument.Document;
+            Dictionary<string, double> Results = [];
+            
+            Stopwatch native = new();
+            native.Start();
+            // Here's the command that will run in both cases:
+            //      It returns a dictionary with all valid elements sorted by family.
+            ICollection<Element> allValidElements = new FilteredElementCollector(doc)
+                        .WhereElementIsNotElementType()
+                        .WhereElementIsViewIndependent()
+                        .ToElements();
+
+            var elementsSortedByFamilyNative = new Dictionary<string, List<long>>();
+            foreach ((Element e, string familyName) in from Element e in allValidElements // create two variables, Element e and string familyType
+                                                       let f = e as FamilyInstance // cast each element as a FamilyInstance
+                                                       where f is not null // check if it's not null
+                                                       let familyName = f.Symbol.Family.Name // assign the family name to the variable familyName
+                                                       select (e, familyName)) // get the variables back
+            {
+                if (!elementsSortedByFamilyNative.TryGetValue(familyName, out List<long>? value))
+                {
+                    value = [];
+                    elementsSortedByFamilyNative[familyName] = value;
+                }
+                value.Add(e.Id.Value);
+            }
+            WriteToFile("elementsByFamily-native.json", elementsSortedByFamilyNative);
+
+            // Add results from native Revit command.
+            native.Stop();
+            Results.Add("Native Revit Command", native.Elapsed.TotalSeconds);
+
+            // ----------------------------------------------------------------------------------------------------
+
+            // Now, the async Direwolf version!
+            Stopwatch direwolf = new();
+            direwolf.Start();
+            RevitTask.Initialize(commandData.Application);
+
+            // Direwolf will:
+            //      - Create a new Howler (in this case, a dispatcher that holds a reference to a Revit document).
+            //      - Load a Howl (it's a set of instructions to perform on a given object. In this case, it'll perform the same thing as above.
+            //      - Create a Wolf (a worker that reads instructions (the Howl) and has a Callback to its summoner (the Howler).
+            //      - When a query is executed, the Howler attaches itself and their respective Howl to each Wolf, and calls the function Run() inside each one.
+            //      - The Wolf then can do anything. It can execute the Howl, or anything else. It has a stack to stash all the things it finds (each one called Catch)
+            //      - If a Howl, when executed, returns any Catch, it *directly* pipes it back to the Wolf through a callback. It does *not* store any data on its own.
+            //      - The Wolf can process any catches when they arrive in the stack. It's in charge of sending Catches up the chain.
+            //      - When all executions are done, the Howler returns a Wolfpack: a serializable bundle of information captured in the process.
+            //      - This Wolfpack can be stored as a JSON object, manipulated further, or anything. It's just a JSON!
+            // TL;DR:
+            //  A Howler attaches Howls to a pack of Wolves, each one with a different mission.
+            //  The instruction they're given mines the data, and pipes it back to the Wolves.
+            //  Each Wolf can process it, or send it directly to the Howler. Then it just becomes a JSON.
+            //
+            //  Howler <-> Wolf -> Howl <-> Endpoint
+            //             + Catches<-|
             Direwolf dw = new();
             dw.ExecuteQueryAsync(new RevitDocumentDispatch(doc), "DocumentInformation");
-
+            
+            // Add results from Direwolf
+            direwolf.Stop();
+            Results.Add("Direwolf Revit Command", direwolf.Elapsed.TotalSeconds);
+            
+            // Show final results
+            TaskDialog t = new("Results")
+            {
+                MainContent = JsonSerializer.Serialize(Results)
+            };
+            t.Show();
             return Result.Succeeded;
         }
 
-        //    public class ResultData
-        //    {
-        //        public ResultData(ExternalCommandData e)
-        //        {
-        //            Command = e;
-        //        }
-        //        private ExternalCommandData Command { get; set; }
+        public void WriteToFile(string filename, object obj)
+        {
+            string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), filename);
+            File.WriteAllText(fileName, JsonSerializer.Serialize(obj));
+        }
 
-        //        public async void Execute()
-        //        {
-        //            RevitTask.Initialize(Command.Application);
 
-        //            var t = await RevitTask.RunAsync(
-        //              () =>
-        //              {
-        //                    Document d = Command.Application.ActiveUIDocument.Document;
-        //                    Direwolf dw = new();
-        //                    dw.ExecuteQueryAsync(new RevitDocumentDispatch(d), "DocumentInformation");
-        //                    dw.ShowResultToGUI();
-        //                    dw.WriteToFile();
-
-        //                  TaskDialog t = new("TestResult");
-        //                  t.MainContent = "Executed";
-        //                  t.Show();
-        //                  return 0;
-        //              }
-        //      );
-        //        }
-        //    }
-        //}
-
-        //public class Reap : ICommand
-        //{
-        //    public bool CanExecute(object parameter) => true;
-        //    public event EventHandler CanExecuteChanged;
-        //    public async void Execute(object parameter)
-        //    {
-        //        var doc = parameter as Document;
-        //        Direwolf dw = new();
-        //        dw.AsyncFetch(new RevitDocumentDispatch(doc)); 
-        //        TaskDialog t = new("Result");
-        //        t.MainContent = dw.GetData();
-        //        t.Show();
-
-        //    }
-        //}
 
         public record class RevitDocumentDispatch : Howler
         {
             public RevitDocumentDispatch(Document revitDoc)
             {
-                GetEditableFamilies h = new(revitDoc);
+                GetElementIdByFamily h = new(revitDoc);
                 CreateWolf(new Wolf(), h);
-                //Dispatch();
             }
         }
 
-        public record class GetEditableFamilies(Document RevitDocument) : Howl
+        public record class GetElementIdByFamily(Document RevitDocument) : Howl
         {
             public override bool Execute()
             {
