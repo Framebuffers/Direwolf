7bd45cae3c5640421336f769e337b571c29f7c4f
Framebuffer <26495556+Framebuffers@users.noreply.github.com>
2025-03-21 19:12:41 -0300

    Started connecting the frontend

---
+++ Direwolf.Revit/Howls/GetDocumentIntrospection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Direwolf.Revit.Howls
+{
+    public record class GetDocumentIntrospection : RevitHowl
+    {
+        public GetDocumentIntrospection(Document doc) => SetRevitDocument(doc);
+    }
+}
---
+++ Direwolf.Revit/Howls/GetFamilyIntrospection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace Direwolf.Revit.Howls
+{
+    public record class GetFamilyIntrospection : RevitHowl
+    {
+        public GetFamilyIntrospection(Document doc) => SetRevitDocument(doc);
+    }
+}
