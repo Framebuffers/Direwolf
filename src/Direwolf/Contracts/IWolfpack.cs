c1653c0add6a2a0fb1b5c0c829a6b4cf931a2ef9
Framebuffer <26495556+Framebuffers@users.noreply.github.com>
2025-05-14 18:14:31 -0400

    EventHook blanks

--- src/Direwolf/Contracts/IWolfpack.cs
+++ src/Direwolf/Contracts/IWolfpack.cs
@@ -1,58 +1,13 @@
-﻿using Direwolf.Parsers;
-using Direwolf.Parsers.Tokens;
+﻿using Direwolf.Dto.Driver;
+using Direwolf.Dto.InternalDb.Enums;
+using Direwolf.Dto.Parser;
 
 namespace Direwolf.Contracts;
 
 public interface IWolfpack
 {
     public DocumentType DocumentType { get; set; }
-    public PlatformType Platform { get; set; }
+    public Realm Realm { get; set; }
     public List<Set> QuerySets { get; set; }
-    public List<Driver> Drivers { get; set; }
+    public List<DriverCommonDto> Drivers { get; set; }
 }
-
-/*
- * # file example
- * wolfpack:
- *      document_type: document
- *      platform: desktop
- *      queries:
- *          set_1:
- *              query_1:
- *                  execution_trigger: none
- *                  target: 
- *                  query_scope: category
- *                      p: *
- *                      op: OfCategory
- *                      q: OST_Walls
- *                  query_scope: family
- *                      operation_1:
- *                          p: name
- *                          op: equals
- *                          q: 'Door_Wood'
- *                  query_scope: parameter
- *                      operation_1:
- *                          p: 'Width'
- *                          op: equals
- *                          q: '200'
- *                      operation_2:
- *                          p: 'Height'
- *                          op: greater_than
- *                          q: '255'
- *                      operation_3:
- *                          p: operation_1
- *                          op: and
- *                          q: operation_2
- *          set_2:
- *              query_2:
- *                  # another set of queries
- *          operation:
- *              p: set_1
- *              op: join
- *              q: set_2
- *      drivers:
- *          json_file:
- *              path: "./result.json"
- *              indented: true
- */
-
