2d13fbd0dccec4489f52d8f83c7559ec54728974
Framebuffer <26495556+Framebuffers@users.noreply.github.com>
2025-05-23 16:45:10 -0400

    Engine is working and generating records from a project.

---
+++ src/Direwolf.Database/EventArgs/TransactionEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.DB;
+
+using Direwolf.Definitions.Internal.Enums;
+
+using Transaction = Direwolf.Definitions.Internal.Transaction;
+
+namespace Direwolf.Database.EventArgs;
+
+public class TransactionEventArgs : System.EventArgs
+{
+    public CrudOperation Operation { get; set; }
+    public Transaction Transaction { get; set; }
+}
