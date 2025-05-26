c3d759d57d0a69d741256f859036fca59989c101
Framebuffer <26495556+Framebuffers@users.noreply.github.com>
2025-05-25 23:23:23 -0400

    Working command button

--- src/Direwolf.Revit/Application.cs
+++ src/Direwolf.Revit/Application.cs
@@ -10,17 +10,23 @@ namespace Direwolf.Revit;
 [UsedImplicitly]
 public class Application : ExternalApplication
 {
+    public static Direwolf? Direwolf;
     public override void OnStartup()
     {
         CreateRibbon();
+        Application.ControlledApplication.DocumentOpened += (sender, args) =>
+        {
+            Direwolf = new Direwolf(args.Document);
+        };
+        
     }
 
     private void CreateRibbon()
     {
         var panel = Application.CreatePanel("Commands",
-                                            "Direwolf.Revit");
+                                            "Direwolf");
 
-        panel.AddPushButton<StartupCommand>("Execute")
+        panel.AddPushButton<PopulateDatabase>("Execute")
              .SetImage(
                  "/Direwolf.Revit;component/Resources/Icons/RibbonIcon16.png")
              .SetLargeImage(
